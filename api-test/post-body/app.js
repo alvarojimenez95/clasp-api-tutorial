4e1452c12e4f73aca8963609d286dc3820c7813f
Alvaro Jimenez <alvaro@Alvaros-Mac-mini.local>
2022-03-10 16:22:43 +0100

    changed post body

--- api-test/post-body/app.js
+++ api-test/post-body/app.js
@@ -1,8 +1,22 @@
 function doPost(event) {
   let body = JSON.parse(event.postData.contents);
+  let data = sayHello(body.name, body.age);
+  let goodBye = sayGoodBye();
   let response = {
     status: 200,
-    request: JSON.stringify(body),
+    request: body,
+    ms2: data,
+    msg1: goodBye,
   };
-  return response;
+  return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(
+    ContentService.MimeType.JSON
+  );
 }
+
+const sayHello = (name, age) => {
+  return `Hello ${name}, you are ${age} years old.`;
+};
+
+const sayGoodBye = () => {
+  return "Goodbye!";
+};
